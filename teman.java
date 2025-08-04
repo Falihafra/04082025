97591850c18570862d34ed7b2c850b42124353dc
Falihafra <faliha_syakira_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:34:14 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,45 @@
+public class teman {
+    
+    public static void main(String[] args) {
+         //variabel -> tipe_data nama_variabel = value;
+        String nama = "Salsabila Abhista";
+        int usia = 16;
+        String cita = "Pengusaha";
+        System.out.println(nama);
+        System.out.println(usia + "tahun");
+        System.out.println(cita);
+
+        String nama2 = "Shabrina";
+        int usia2 = 16;
+        String cita2 = "Bos sukses";
+        System.out.println(nama2);
+        System.out.println(usia2 + "tahun");
+        System.out.println(cita2);
+
+        String nama3 = "Tazkia Adila Affandie";
+        int usia3 = 15;
+        String cita3 = "Graphic Designer";
+        System.out.println(nama3);
+        System.out.println(usia3 + "tahun");
+        System.out.println(cita3);
+
+        String nama4 = "Zian Mahdia";
+        int usia4 = 15;
+        String cita4 = "Pengusaha";
+        System.out.println(nama4);
+        System.out.println(usia4 + "tahun");
+        System.out.println(cita4);
+
+        String nama5 = "Azka Rullah";
+        int usia5 = 15;
+        String cita5 = "Web Developer";
+        System.out.println(nama5);
+        System.out.println(usia5 + "tahun");
+        System.out.println(cita5);
+
+
+
+
+
+    }
+}
