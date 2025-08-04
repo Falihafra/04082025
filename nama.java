4aecc39e7b8c07bd90df23316692cb04597f5593
Falihafra <faliha_syakira_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:17:00 +0700

    Add files via upload

--- nama.java
+++ nama.java
@@ -1,6 +1,14 @@
 public class nama {
-public static void main(String[] args) {
-    System.out.println("Faliha Afra Syakira");
-}
+    public static void main(String[] args) { 
+         //main method
+         System.out.println("Faliha Afra Syakira");
+         System.out.println("SMK Telkom Malang");
+         System.out.println("Sekolah Alam Indonesia");
+         System.out.println("Tri Sakti");
+         System.out.println("Yeni Dwi");
+         System.out.println("Traveller");
+         System.out.println("Bersepeda");
+
+    }
 }
 
